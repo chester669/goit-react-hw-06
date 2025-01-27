259a4acad993e2276e63014b841238b53b7119b4
chester669 <viktorostash44@gmail.com>
2025-01-27 16:31:38 +0200

    edit  2.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux"; // Додаємо Provider
-import { PersistGate } from "redux-persist/integration/react"; // Додаємо PersistGate
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import { store, persistor } from "./redux/store";
 import "./index.css";
